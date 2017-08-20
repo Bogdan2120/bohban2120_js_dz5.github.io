4274a74dd0b992f6e09c33e430ef60bb020f25ad
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2017-08-20 15:12:28 +0300

    homeworck_3

--- 3/js.js
+++ 3/js.js
@@ -1,15 +1,15 @@
 let obj = {
-    str : document.querySelector('input').value,
+    str : document.querySelector('input'),
     button : document.querySelector('button'),
     p : document.querySelector('p')
 }
 obj.button.addEventListener('click', function () {
   
-    if (~obj.str.indexOf("http://")) {
-        obj.p.innerHTML = obj.str.substring(7);
+    if (~obj.str.value.indexOf("http://")) {
+        obj.p.innerHTML = obj.str.value.substring(7);
     } 
-    else if (~obj.str.indexOf("https://")) {
-        obj.p.innerHTML = obj.str.substring(8);
+    else if (~obj.str.value.indexOf("https://")) {
+        obj.p.innerHTML = obj.str.value.substring(8);
     } 
     else {
         alert ('Введите коректно URL адрес ')
